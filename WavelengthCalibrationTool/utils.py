023dc5d75e33e0b1b13b81c99989b05b86db6a46
Joe Philip Ninan <indiajoe@gmail.com>
2017-07-12 01:10:01 -0400

    Added optional argument to provide fitting window range

--- WavelengthCalibrationTool/utils.py
+++ WavelengthCalibrationTool/utils.py
@@ -6,21 +6,37 @@ from astropy.modeling import models, fitting
 
 def NearestIndex(Array,value):
     """ Returns the index of element in numpy 1d Array nearest to value """
+    #return np.searchsorted(Array,value)  # Works for sorted array only
     return np.abs(Array-value).argmin()
 
-def FitLineToData(SpecX,SpecY,Pos,Amp,AmpisBkgSubtracted=True,Sigma = 3):
+
+def FitLineToData(SpecX,SpecY,Pos,Amp,AmpisBkgSubtracted=True,Sigma = 3, WindowStartEnd = None):
     """ Fits a model line to the SpecX SpecY data 
     Model as well as parameters to fit are defined inside this function
     Sigma = 3 # Approximate size of the line width sigma in pixels.
-    Half of the window size to use for fitting line in pixels will be FitWindowH = 5*Sigma  """
+    If WindowStartEnd = None
+          Half of the window size to use for fitting line in pixels will be FitWindowH = 5*Sigma  
+    else WindowStartEnd = (StartW,EndW)
+         will result in data in the wavelength range StartW to EndW to be used for fitting
+    """
 
-    FitWindowH = 5*Sigma  # window to fit the line is 5* expected Sigma of line
-    StartIdx = NearestIndex(SpecX,Pos) - FitWindowH
-    EndIdx = NearestIndex(SpecX,Pos) + FitWindowH
-    SliceToFitX = SpecX[StartIdx:EndIdx+1] 
-    SliceToFitY = SpecY[StartIdx:EndIdx+1]
-    MedianBkg = np.median(SliceToFitY)
-    dw = np.abs(np.median(SliceToFitX[1:]-SliceToFitX[:-1]))
+    if WindowStartEnd is None:
+        FitWindowH = 5*Sigma  # window to fit the line is 5* expected Sigma of line
+        StartIdx = NearestIndex(SpecX,Pos) - FitWindowH
+        EndIdx = min(NearestIndex(SpecX,Pos) + FitWindowH +1, len(SpecX)-1)
+    else:
+        StartIdx = NearestIndex(SpecX,WindowStartEnd[0])
+        EndIdx = min(NearestIndex(SpecX,WindowStartEnd[1]) +1, len(SpecX)-1)
+
+    SliceToFitX = SpecX[StartIdx:EndIdx] 
+    SliceToFitY = SpecY[StartIdx:EndIdx]
+
+    if WindowStartEnd is None:
+        MedianBkg = np.median(SliceToFitY)
+    else:
+        MedianBkg = np.median(SliceToFitY[[0,-1]]) # median of first and last values in the window
+
+    dw = np.abs(np.median(np.diff(SliceToFitX)))
     if not AmpisBkgSubtracted:
         Amp = Amp - MedianBkg
 
