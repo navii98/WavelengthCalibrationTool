a7cfd43ee3da957efd77d25b2bfba7287a2aa392
indiajoe <indiajoe@gmail.com>
2017-07-25 03:15:07 +0530

    Added setup.py for easy install via pip

--- README.rst
+++ README.rst
@@ -3,3 +3,5 @@ Wavelength Calibration Tool
 
 This standalone tool is to help in both interactive and non-interactive wavelength calibration of arc spectra.
 
+## Command Line Tools
++ `iidentify` : Interactive tool to identify and fit dispersion solution to raw arc spectrum
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='WavelengthCalibrationTool',
+      version='0.1',
+      description='Tool for doing wavelength calibration of arc spectrum',
+      long_description = readme(),
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Astronomy',
+      ],
+      keywords='Wavelength Calibration Spectrum Astronomy',
+      url='https://github.com/indiajoe/WavelengthCalibrationTool',
+      author='Joe Ninan',
+      author_email='indiajoe@gmail.com',
+      license='GPLv3+',
+      packages=['WavelengthCalibrationTool'],
+      entry_points = {
+          'console_scripts': ['iidentify=WavelengthCalibrationTool.iidentify:main'],
+      },
+      install_requires = [
+          'numpy',
+          'matplotlib',
+          'readline',
+          'astropy',
+      ],
+      include_package_data=True,
+      zip_safe=False)
